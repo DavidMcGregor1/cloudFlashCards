b29b9d2d2a333bf2bef22cc0d57b56799cf2fd96
David.McGregor <David.McGregor@hl.co.uk>
2023-11-01 15:20:09 +0000

    minor logic changes

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-console.log("Test");
-
 const flashcards = [
-  { term: "Term 1", definition: "Definiton 1" },
-  { term: "Term 2", definition: "Definiton 2" },
-  { term: "Term 3", definition: "Definiton 3" },
-  { term: "Term 4", definition: "Definiton 4" },
-  { term: "Term 5", definition: "Definiton 5" },
+  { term: "test0", definition: "test0 def" },
+  { term: "test1", definition: "test1 def" },
+  { term: "test2", definition: "test2 def" },
+  { term: "test3", definition: "test3 def" },
+  { term: "test4", definition: "test4 def" },
+  { term: "test5", definition: "test5 def" },
+  { term: "test6", definition: "test6 def" },
+  { term: "test7", definition: "test7 def" },
 ];
 
 const termElement = document.getElementById("term");
 const definitionElement = document.getElementById("definition");
-const showDefinitionButton = document.getElementById("show-definition-button");
-const nextCardButton = document.getElementById("next-card-button");
-const flashcard = document.querySelector(".flashcard");
+const showDefinitionBtn = document.getElementById("show-definition-button");
+const nextCardBtn = document.getElementById("next-card-button");
 const randomOrderCheckbox = document.getElementById("random-order-checkbox");
+const flashcard = document.querySelector(".flashcard");
 
 let currentCardIndex = 0;
 let shuffledIndices = [...Array(flashcards.length).keys()]; // Create an array of indices to shuffle
 let isRandomOrder = false;
+
 showFront();
 
-showDefinitionButton.addEventListener("click", showBack);
-nextCardButton.addEventListener("click", nextCard);
+showDefinitionBtn.addEventListener("click", showBack);
+nextCardBtn.addEventListener("click", nextCard);
 randomOrderCheckbox.addEventListener("change", toggleRandomOrder);
 
 function showFront() {
   flashcard.style.transform = "rotateY(0deg)";
-  termElement.textContent = flashcards[currentCardIndex].term;
+  termElement.textContent = flashcards[shuffledIndices[currentCardIndex]].term;
 }
 
 function showBack() {
   flashcard.style.transform = "rotateY(180deg)";
-  definitionElement.textContent = flashcards[currentCardIndex].definition;
+  definitionElement.textContent =
+    flashcards[shuffledIndices[currentCardIndex]].definition;
 }
 
 function nextCard() {
-  currentCardIndex = (currentCardIndex + 1) % flashcards.length;
+  currentCardIndex++;
+  if (currentCardIndex >= flashcards.length) {
+    currentCardIndex = 0;
+  }
   showFront();
 }
 
 function toggleRandomOrder() {
   isRandomOrder = randomOrderCheckbox.checked;
-  randomOrderCheckbox.disabled = true; // Disable the checkbox while shuffling
-
+  randomOrderCheckbox.disabled = true;
   if (isRandomOrder) {
     shuffledIndices = shuffleArray([...Array(flashcards.length).keys()]);
   } else {
