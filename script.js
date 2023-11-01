ade30c884a779d4704be70a463026166c555ac12
David.McGregor <David.McGregor@hl.co.uk>
2023-11-01 15:01:50 +0000

    declared array with 5 mock flash cards, declared base constants and card index

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Test");
+
+const flashcards = [
+  { term: "Term 1", definition: "Definiton 1" },
+  { term: "Term 2", definition: "Definiton 2" },
+  { term: "Term 3", definition: "Definiton 3" },
+  { term: "Term 4", definition: "Definiton 4" },
+  { term: "Term 5", definition: "Definiton 5" },
+];
+
+const termElement = document.getElementById("term");
+const definitionElement = document.getElementById("definition");
+const showDefinitionBtn = document.getElementById("show-definition-button");
+const nextCardBtn = document.getElementById("next-card-button");
+const flashcard = document.querySelector(".flashcard");
+
+let currentCardIndex = 0;
